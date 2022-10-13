556b05b77779c29ff326bc84b84e4cbaf07d9148
panca00 <hugohenrique1998@gmail.com>
2022-10-13 18:19:52 -0300

    content create

---
+++ src/app/pages/content/content.component.ts
@@ -0,0 +1,42 @@
+import { ThisReceiver } from '@angular/compiler';
+import { Component, Input, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { dataFake } from 'src/app/data/dataFake';
+
+@Component({
+  selector: 'app-content',
+  templateUrl: './content.component.html',
+  styleUrls: ['./content.component.css']
+})
+export class ContentComponent implements OnInit {
+  
+
+  photoCover:string=""
+  contentTitle:string=""
+  contentDescription:string=""
+  private id:string|null="0"
+
+  constructor(private route:ActivatedRoute) { 
+
+  }
+
+  ngOnInit(): void {
+    this.route.paramMap.subscribe(
+      value => this.id= value.get("Id")
+      
+    )
+    this.setValuesToComponent(this.id)
+  
+  }
+
+  setValuesToComponent(id:string|null){
+    const result = dataFake.filter(article=>
+        article.Id==id
+      )[0]
+      
+    this.contentTitle=result.title
+    this.contentDescription=result.description
+    this.photoCover=result.photo
+  }
+
+}
